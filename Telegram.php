392d9d02e66c9fdfef408504276650f65f30e963
jane <cheshirenok@gmail.com>
2016-06-22 20:55:17 +0500

    first commit

---
+++ Telegram.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace militska\telegram;
+
+use GuzzleHttp\Client;
+require 'vendor/autoload.php';
+require 'config.php';
+require 'Common.php';
+
+class Telegram
+{
+    use \Common;
+
+
+    /**
+     * todo Перенести в бд
+     * @return string
+     */
+    public function getHello()
+    {
+        return $this->how[array_rand($this->how, 1)];
+
+    }
+
+    /**
+     * todo Перенести в бд
+     * @return string
+     */
+    public function getComplement()
+    {
+        return $this->complements[array_rand($this->complements, 1)];
+
+    }
+
+    const API_URL_SEND_MESSAGE = 'https://api.telegram.org/bot%s/sendMessage';
+
+    /**
+     * @param integer $chatId
+     * @param string $message
+     */
+    public function sendMessage($chatId, $message)
+    {
+        $telegram = new self;
+        $telegram->apiRequest(self::API_URL_SEND_MESSAGE,
+            array('chat_id' => $chatId, "text" => $message));
+    }
+
+    /**
+     * @param string $method
+     * @param array $parameters
+     * @return bool
+     */
+    public function apiRequestWebHook($method, $parameters)
+    {
+        if (!is_string($method)) {
+            error_log("Method name must be a string\n");
+            return false;
+        }
+
+        if (!$parameters) {
+            $parameters = array();
+        } else {
+            if (!is_array($parameters)) {
+                error_log("Parameters must be an array\n");
+                return false;
+            }
+        }
+
+        $parameters["method"] = $method;
+
+        header("Content-Type: application/json");
+        echo json_encode($parameters);
+        return true;
+    }
+
+
+    /**
+     * @param string $method
+     * @param array $parameters
+     * @return bool|mixed|\Psr\Http\Message\ResponseInterface
+     */
+    public function apiRequest($method, $parameters = null)
+    {
+        if (!is_string($method)) {
+            error_log("Method name must be a string\n");
+            return false;
+        }
+
+        if (!is_array($parameters)) {
+            error_log("Parameters must be an array\n");
+            return false;
+        }
+
+        foreach ($parameters as $key => &$val) {
+            if (!is_numeric($val) && !is_string($val)) {
+                $val = json_encode($val);
+            }
+        }
+        $url = sprintf($method, Configure::TOKEN);
+        $client = new Client();
+
+        return $client->request('GET', $url, ['query' => $parameters, 'timeout' => 60, 'connect_timeout' => 5]);
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+
+require 'Telegram.php';
+use militska\telegram\Telegram;
+
+if (!$update) {
+    // receive wrong update, must not happen
+    exit;
+}
+
+if (isset($update["message"])) {
+    processMessage($update["message"]);
+}
+
+function processMessage($message)
+{
+    $telegram = new Telegram();
+    $chat_id = $message['chat']['id'];
+    if (isset($message['text'])) {
+        // incoming text message
+        $text = $message['text'];
+        switch ($text) {
+            case "/start" :
+                $telegram->apiRequest(Telegram::API_URL_SEND_MESSAGE, [
+                    'chat_id' => $chat_id,
+                    "text" => $message['chat']['first_name']  . ', ' .$telegram->getHello(),
+                    'reply_markup' => [
+                        'one_time_keyboard' => false,
+                        'keyboard' => [['Время', 'Хочу общаться']],
+                        'resize_keyboard' => true
+                    ]
+                ]);
+                break;
+            case  "Время" :
+                $telegram->sendMessage($chat_id, 'Сейчас ' . date('d.m.Y H:i', time()));
+                break;
+            case  "Хочу общаться" :
+                $telegram->apiRequest(Telegram::API_URL_SEND_MESSAGE, array(
+                    'chat_id' => $chat_id,
+                    "text" => 'О чем мы можем пообщаться?',
+                    'reply_markup' => [
+                        'one_time_keyboard' => false,
+                        'keyboard' => array(array('Нужен комплимент', 'Все плохо')),
+                        'resize_keyboard' => true
+                    ]
+                ));
+                break;
+            case 'Нужен комплимент' :
+                $telegram->sendMessage($chat_id, "Вероятно, ты довольно неплохо выглядешь");
+                break;
+            case 'Средне плохо' :
+                $telegram->sendMessage($chat_id, 'Средне плохо? Точно средне?');
+                break;
+            case 'Очень плохо' :
+                $telegram->sendMessage($chat_id, 'Ты там держись, всего хорошего тебе!');
+                break;
+            case 'Все плохо' :
+                $telegram->apiRequest(Telegram::API_URL_SEND_MESSAGE, array(
+                    'chat_id' => $chat_id,
+                    "text" => "Привет",
+                    'reply_markup' => [
+                        'one_time_keyboard' => true,
+                        'keyboard' => array(array('Средне плохо', 'Очень плохо')),
+                        'resize_keyboard' => true
+                    ]
+                ));
+                break;
+            default :
+                if (substr($text, 0, 5) == "/call") {
+                    $rest = substr($text, 6);    // возвращает "f"
+                    $telegram->apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => $rest));
+                    break;
+                }
+
+                $telegram->apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => "Не понимаю о чем ты"));
+                break;
+
+        }
+    } else {
+        $telegram->apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'));
+    }
+}
+
